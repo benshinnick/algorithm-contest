855ffc048c12261cae5e8457934e25adea8a0af3
Ben Shinnick <shinnickbenjamin@gmail.com>
2021-12-13 17:47:01 -0600

    Removed the menu page and added those buttons to the header for a better user experience

--- src/AlgoContest/AlgoContest.jsx
+++ src/AlgoContest/AlgoContest.jsx
@@ -12,32 +12,35 @@ export default class AlgoContest extends React.Component {
     }
 
     componentDidMount() {
-        ReactDOM.render(
-            <div id='homemenu'>
-                <button className='menubutton' onClick={() => this.changePageContentToSortContest()}>Sorting Contest</button>
-                <button className='menubutton' onClick={() => this.changePageContentToGraphContest()}>Graph Contest</button>
-                <button className='menubutton' onClick={() => this.changePageContentToProjectInfo()}>Project Info</button>
-            </div>, document.getElementById('pagecontent'));
+        this.changePageContentToSortContest();
     }
-
-    changePageContentToProjectInfo() {
-        ReactDOM.render(<div>Project Where You Can Watch Algorithms Compete Against Each Other</div>, document.getElementById('pagecontent'));
-    } 
 
     changePageContentToSortContest() {
         this.setState({ algorithmType: 'sort' });
+
+        document.querySelector('#sortcontestbutton').style.backgroundColor = '#318fac';
+        document.querySelector('#graphcontestbutton').style.backgroundColor = 'transparent';
+
         ReactDOM.render(<div>Sort</div>, document.getElementById('pagecontent'));
     }
 
     changePageContentToGraphContest() {
         this.setState({ algorithmType: 'graph' });
+
+        document.querySelector('#sortcontestbutton').style.backgroundColor = 'transparent';
+        document.querySelector('#graphcontestbutton').style.backgroundColor = '#318fac';
+        
         ReactDOM.render(<div>Graph</div>, document.getElementById('pagecontent'));
     }
 
     render() {
         return (
             <div id='algocontest'>
-                <div id='algocontestheader'><a href="http://localhost:3000/">AlgoContest</a></div>
+                <div id='algocontestheader'>
+                    <a href="http://localhost:3000/">AlgoContest</a>
+                    <button id='sortcontestbutton' onClick={() => this.changePageContentToSortContest()}>Sorting Contest</button>
+                    <button id='graphcontestbutton' onClick={() => this.changePageContentToGraphContest()}>Graph Contest</button>
+                </div>
                 <div id='pagecontent'></div>
             </div>
         );
