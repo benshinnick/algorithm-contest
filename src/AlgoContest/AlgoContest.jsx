a4d05ac65b4e80d64fa526f068b4f180ae075e2f
Ben Shinnick <shinnickbenjamin@gmail.com>
2021-12-13 16:56:09 -0600

    Created the menu page for the application

--- src/AlgoContest/AlgoContest.jsx
+++ src/AlgoContest/AlgoContest.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './AlgoContest.css';
 
 export default class AlgoContest extends React.Component {
@@ -6,13 +7,39 @@ export default class AlgoContest extends React.Component {
         super(props);
 
         this.state = {
-            algorithmType: 'sorting'
+            algorithmType: ''
         };
+    }
+
+    componentDidMount() {
+        ReactDOM.render(
+            <div id='homemenu'>
+                <button className='menubutton' onClick={() => this.changePageContentToSortContest()}>Sorting Contest</button>
+                <button className='menubutton' onClick={() => this.changePageContentToGraphContest()}>Graph Contest</button>
+                <button className='menubutton' onClick={() => this.changePageContentToProjectInfo()}>Project Info</button>
+            </div>, document.getElementById('pagecontent'));
+    }
+
+    changePageContentToProjectInfo() {
+        ReactDOM.render(<div>Project Where You Can Watch Algorithms Compete Against Each Other</div>, document.getElementById('pagecontent'));
+    } 
+
+    changePageContentToSortContest() {
+        this.setState({ algorithmType: 'sort' });
+        ReactDOM.render(<div>Sort</div>, document.getElementById('pagecontent'));
+    }
+
+    changePageContentToGraphContest() {
+        this.setState({ algorithmType: 'graph' });
+        ReactDOM.render(<div>Graph</div>, document.getElementById('pagecontent'));
     }
 
     render() {
         return (
-            <div>AlgoContest</div>
+            <div id='algocontest'>
+                <div id='algocontestheader'><a href="http://localhost:3000/">AlgoContest</a></div>
+                <div id='pagecontent'></div>
+            </div>
         );
     }
 }
